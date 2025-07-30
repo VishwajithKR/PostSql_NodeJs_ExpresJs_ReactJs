1153ba9110045ed736c12e292b511c9d70626873
vishwajith <vishwakr1998@gmail.com>
2025-07-30 13:34:36 +0530

    checking

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,7 +7,6 @@ import { store } from "./redux/store/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
